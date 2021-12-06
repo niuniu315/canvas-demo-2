46a6bb486ca62158dc8e666096a6f709aea7e6c1
niuniu315 <454378439@qq.com>
2021-12-06 23:34:39 +0800

    优化

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
-canvas.onclick = (e) => {
-  let div = document.createElement('div')
-  div.className = "dot"
-  div.style.left = e.clientX + 'px'
-  div.style.top = e.clientY + 'px'
+let canvas = document.getElementById("canvas");
+canvas.width = document.documentElement.clientWidth
+canvas.height = document.documentElement.clientHeight
+let ctx = canvas.getContext("2d");
 
-  console.log(div.className);
-  canvas.appendChild(div)
+ctx.fillStyle = "red";
+ctx.strokeStyle = "black";
+ctx.lineWidth = 4;
+ctx.lineCap = "round";
+let painting = false
+let last
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+}
+//是否是触摸设备
+let isTouchDevice = "ontouchstart" in document.documentElement;
+if (isTouchDevice){
+  canvas.ontouchmove = (e) => {
+    let x = e.touches[0].clientX;
+    let y = e.touches[0].clientY;
+    ctx.beginPath();
+    ctx.arc(x, y, 1, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill()
+  }
+}else {
+  canvas.onmousedown = (e) =>{
+    painting = true
+    last = [e.clientX, e.clientY]
+  }
+  canvas.onmousemove = (e) => {
+    if (painting === true){
+      drawLine(last[0], last[1], e.clientX, e.clientY);
+      last = [e.clientX, e.clientY]
+    } else {}
+  }
+  canvas.onmouseup = () => {
+    painting = false
+  }
 }
